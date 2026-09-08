937c4e86b0de87ebcc9cb948da43f74cff385914
RizziSilva <rizzi.william@hotmail.com>
2026-09-08 08:27:26 -0300

    feat: create rejoin event and change to use user id from firebase to map players

--- src/entities/player.entity.ts
+++ src/entities/player.entity.ts
@@ -1,10 +1,12 @@
 import { PlayerSymbol } from '@types';
 
 export class Player {
+  playerId: string;
   socketId: string;
   symbol: PlayerSymbol;
 
-  constructor(socketId: string, symbol: PlayerSymbol) {
+  constructor(playerId: string, socketId: string, symbol: PlayerSymbol) {
+    this.playerId = playerId;
     this.socketId = socketId;
     this.symbol = symbol;
   }
